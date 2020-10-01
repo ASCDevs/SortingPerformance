941ab3aeedbdc59f1c49f68578632a6f2e96c759
Alexandre Santos <ale.santos49@gmail.com>
2020-09-30 23:08:37 -0300

    Main file to compile

---
+++ Principal.cpp
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <new>
+
+int comparacoes=0;
+int movimentacoes=0;
+
+#include "MergeSort.h"
+#include "QuickSort.h"
+#include "ShellSort.h"
+#include "dados.h"
+
+void menu();
+void encaminhaOpcao(char op);
+
+int main(){
+
+    menu();
+
+}
+
+void menu(){
+
+    char opcao;
+    
+    do{
+        
+        puts("------Performance-de-Ordenacao------");
+        puts("1 - Testes Ordernacao");
+        //puts("2 - Merge Sort");
+        //puts("3 - Quick Sort");
+        //puts("4 - Shell Sort");
+        puts("0 - Sair");
+        scanf("%c",&opcao);
+
+        encaminhaOpcao(opcao);
+        
+        system("cls||clear");
+
+    }while(opcao!='0');
+
+    puts("Saida do sistema\n");
+    system("pause");
+}
+
+void encaminhaOpcao(char op){
+
+    switch (op){
+        case '1':
+            system("cls||clear");
+            menuTestesOrdenacao();
+            break;
+    }
+}
+
+
+void menuTestesOrdenacao(){
+
+    char op;
+
+    do{
+        system("cls||clear");
+        puts("1 - Digitar numeros");
+        puts("2 - Ler arquivo txt");
+        puts("3 - Dados randomicos (500 inteiros)");
+        puts("4 - Dados randomicos (50000 inteiros)");
+        puts("5 - Dados randomicos (100000 inteiros)");
+        puts("6 - Digitar quantidade de numeros randomicos");
+        puts("0 - Voltar");
+        scanf("%c",&op);
+        encaminhaOpcaoDados(op);
+
+    }while(op!='0');
+}
+
+void encaminhaOpcaoDados(char op){
+
+    switch (op){
+        case '1':
+            system("cls");
+            lerEntradaUser();
+            puts("");
+            system("pause");
+            break;
+        case '2':
+            system("cls");
+            lerArquivo();
+            system("pause");
+            break;
+        case '3':
+            system("cls");
+            geraVetor(vetorIntP,500,"random");
+            system("pause");
+            break;
+        case '4':
+            system("cls");
+            geraVetor(vetorIntM,5000,"random");
+            system("pause");
+            break;
+        case '5':
+            system("cls");
+            geraVetor(vetorIntG,100000,"random");
+            system("pause");
+            break;
+        case '6':
+            system("cls");
+            entradaUserRandom();
+            system("pause");
+            break;
+    }
+}
